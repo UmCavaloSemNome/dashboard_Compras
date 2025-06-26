807e100555ff33b1ca685798f1ac39c265c28c3e
UmCavaloSemNome <jannuzzi.rapha@gmail.com>
2025-06-25 22:00:29 -0300

    Solução definitiva: Corrige package.json e reinstala dependências

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: { extend: {} },
   plugins: [],
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> b9ce6113f22f45a3d5768504998a3672d5e54b89
